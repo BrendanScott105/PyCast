15b91990acdf2572caa3229966af1fae5ee7ae24
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2020-06-16 18:58:27 -0400

    Tkinter doesn't fucking work, its a fucking abomination of a library and I don't know why there is not an alternative

--- RayCast.py
+++ RayCast.py
@@ -1,5 +1,3 @@
-import tkinter
-
 ########################
 #### Start settings ####
 ########################
@@ -11,11 +9,11 @@ R04 = [1,1,1,0,1,0,0,0,0,0,0,0,1]             #██████  ██       
 R05 = [1,0,1,0,1,0,1,0,0,0,0,0,1]             #██  ██  ██  ██          ██
 R06 = [1,0,0,0,0,0,1,0,0,0,0,0,1]             #██          ██          ██
 R07 = [1,0,1,1,1,1,1,1,1,0,1,1,1]             #██  ██████████████  ██████
-R08 = [1,0,1,0,1,0,1,1,0,0,0,1,1]             #██  ██  ██  ████      ████
-R09 = [1,0,0,0,0,0,1,0,0,1,0,0,1]             #██          ██    ██    ██
-R10 = [1,1,0,1,0,0,0,0,1,1,1,0,1]             #████  ██        ██████  ██
-R11 = [1,0,0,0,0,0,1,0,0,1,0,0,1]             #██          ██    ██    ██
-R12 = [1,0,1,0,1,0,1,1,0,0,0,1,1]             #██  ██  ██  ████      ████
+R08 = [1,0,1,0,1,0,1,0,0,0,0,1,1]             #██  ██  ██  ██        ████
+R09 = [1,0,0,0,0,0,0,0,0,1,0,0,1]             #██                ██    ██
+R10 = [1,1,0,1,0,1,0,0,1,0,0,0,1]             #████  ██  ██    ██      ██
+R11 = [1,0,0,0,0,0,0,0,0,1,0,0,1]             #██                ██    ██
+R12 = [1,0,1,0,1,0,1,0,0,0,0,1,1]             #██  ██  ██  ██        ████
 R13 = [1,1,1,1,1,1,1,1,1,1,1,1,1]             #██████████████████████████
 #Individual map rows for easy modification     ^ Above is a better view ^
 CeilingColor = "#0000FF"
@@ -23,13 +21,11 @@ FloorColor = "#FF0000"
 #The above two are in Hexadecimal [ie #7F289A] or a supported color name
 #Supported color list : http://www.tcl.tk/man/tcl8.5/TkCmd/colors.htm
 PlayerSpeed = .25 #In terms of map units
-TurnSpeed = 1/360 #In terms of fraction of a full circle [1/360 = 1 deg.]
+TurnSpeed = 1/360 #In terms of fractions of a full circle [1/360 = 1 deg.]
 WindowLength = 500 #In pixels [Also equal to number of rays cast]
-WindowHeight = 500 #In pixels [Multiplicative value for the calculated heights]
-FOV = 1/2000 #Distance between rays, Idealy, set it to 1/[WindowLength*4] for a 90 degree view, also in fractions of a full circle
+WindowHeight = 500 #In pixels [Multiplicative value for the heights]
+FOV = 1/2000 #Distance between rays in fractions of a circle, 1/[WindowLength*4] = 90 deg. 
 
-#################################
-#### Start main Raycast code ####
-#################################
-
-Map = [R01,R02,R03,R04,R05,R06,R07,R08,R09,R10,R11,R12,R13]
+###############################
+#### Start window creation ####
+###############################

bdd679d81808c4d5fcbf3c1aa6b0e1e2ef40e33c
Brendan Scott <56208490+BrendanScott105@users.noreply.github.com>
2020-06-19 03:29:15 -0400

    Begin forwards movement and make map easier to expand

--- RayCast.py
+++ RayCast.py
@@ -1,26 +1,26 @@
 import pygame
 import keyboard
 import time
-from math import *
+from math import * #For Canvas, Keypresses, Speed, and various math functions
 
 ########################
 #### Start settings ####
 ########################
-
-R01 = [1,1,1,1,1,1,1,1,1,1,1,1,1]             #██████████████████████████
-R02 = [1,0,0,0,1,0,1,0,0,0,0,0,1]             #██      ██  ██          ██
-R03 = [1,0,1,0,1,0,1,0,0,0,0,0,1]             #██  ██  ██  ██          ██
-R04 = [1,1,1,0,1,0,0,0,0,0,0,0,1]             #██████  ██        []    ██
-R05 = [1,0,1,0,1,0,1,0,0,0,0,0,1]             #██  ██  ██  ██          ██
-R06 = [1,0,0,0,0,0,1,0,0,0,0,0,1]             #██          ██          ██
-R07 = [1,0,1,1,1,1,1,1,1,0,1,1,1]             #██  ██████████████  ██████
-R08 = [1,0,1,0,1,0,1,0,0,0,0,0,1]             #██  ██  ██  ██          ██
-R09 = [1,0,0,0,0,0,0,0,1,1,1,0,1]             #██              ██████  ██
-R10 = [1,0,1,0,1,0,1,0,1,0,0,0,1]             #██  ██  ██  ██  ██      ██
-R11 = [1,0,0,0,0,0,0,0,1,1,1,0,1]             #██              ██████  ██
-R12 = [1,0,1,0,1,0,1,0,0,0,0,1,1]             #██  ██  ██  ██          ██
-R13 = [1,1,1,1,1,1,1,1,1,1,1,1,1]             #██████████████████████████
-#Individual map rows for easy modification     ^ Above is a better view ^
+Map = [
+[1,1,1,1,1,1,1,1,1,1,1,1,1],             #██████████████████████████
+[1,0,0,0,1,0,1,0,0,0,0,0,1],             #██      ██  ██          ██
+[1,0,1,0,1,0,1,0,0,0,0,0,1],             #██  ██  ██  ██          ██
+[1,1,1,0,1,0,0,0,0,S,0,0,1],             #██████  ██        []    ██
+[1,0,1,0,1,0,1,0,0,0,0,0,1],             #██  ██  ██  ██          ██
+[1,0,0,0,0,0,1,0,0,0,0,0,1],             #██          ██          ██
+[1,0,1,1,1,1,1,1,1,0,1,1,1],             #██  ██████████████  ██████
+[1,0,1,0,1,0,1,0,0,0,0,0,1],             #██  ██  ██  ██          ██
+[1,0,0,0,0,0,0,0,1,1,1,0,1],             #██              ██████  ██
+[1,0,1,0,1,0,1,0,1,0,0,0,1],             #██  ██  ██  ██  ██      ██
+[1,0,0,0,0,0,0,0,1,1,1,0,1],             #██              ██████  ██
+[1,0,1,0,1,0,1,0,0,0,0,1,1],             #██  ██  ██  ██          ██
+[1,1,1,1,1,1,1,1,1,1,1,1,1]]             #██████████████████████████
+#When changing the map, always enclose it, otherwise the program is likely to close/hang
 CeilingColor = "#0000FF"
 FloorColor = "#FF0000"
 #The above two are in Hexadecimal [ie #7F289A] or a supported color name
@@ -36,22 +36,23 @@ FOV = 75 #In terms of degrees
 #############################################################
 
 Rotation = 0
-X,Y = 0,0
+X,Y = 0,0 #Initialize positional and rotational variables
 pygame.init()
 pygame.display.set_caption("Python raycasting demo © Brendan Scott")
-Canvas = pygame.display.set_mode((WindowLength, WindowHeight))
-while (keyboard.is_pressed("escape") == False):
+Canvas = pygame.display.set_mode((WindowLength, WindowHeight)) #Create canvas
+while (keyboard.is_pressed("escape") == False): #Halt program on pressing "Esc"
 	try:
-		if keyboard.is_pressed("up"):
-			print("To be added")
-		if keyboard.is_pressed("down"):
-			print("To be added")
-		if keyboard.is_pressed("left"):
+		if keyboard.is_pressed("up"): #Realistically this should give proper values
+			DeltaY = sin(Rotation)*PlayerSpeed
+			print(DeltaY)
+			DeltaX = sqrt(abs(pow(PlayerSpeed,2)-pow(DeltaY,2)))
+			print(DeltaX)
+		if keyboard.is_pressed("left"): #Decrements rotation and loops back to 360
 			if Rotation <= 0 :
 				Rotation = 360 - TurnSpeed
 			else : Rotation = Rotation - TurnSpeed
 			print(Rotation)
-		if keyboard.is_pressed("right"):
+		if keyboard.is_pressed("right"): #Increments rotation up to 360
 			Rotation = (Rotation + TurnSpeed) % 360
 			print(Rotation)
 		time.sleep(.01)
